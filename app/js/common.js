23ca4bcfe377b7b9fc1bd6e428c9959d06a78198
bstroshek <bstroshek@gmail.com>
2017-11-25 19:27:46 +0200

    main-page

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,60 @@
 $(function() {
 
 
-	// Selectize
+    /*Selectize*/
 
     $('select').selectize({
         create: true,
     });
+
+    /*SLICK*/
+
+    $('.single-item').slick({
+        arrows: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                }
+            }
+        ]
+    });
+
+    /*END SLICK*/
+
+    // Fancybox
+
+    $("[data-fancybox]").fancybox();
+
+
+
+    // Ajax form
+
+    $('.popup__form').submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: th.serialize()
+        }).done(function() {
+            setTimeout(function() {
+                th.trigger("reset");
+                $(th).find('.button').addClass('success-active');
+                $(".button").fancybox();
+            }, 1000);
+        });
+        return false;
+    });
+
+
+
+
 
 
 });
